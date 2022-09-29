0ae5d27eb0c76ed920ae79ec9ffd09ec8d55a71c
AntoinePrv <AntoinePrv@users.noreply.github.com>
2022-09-29 10:45:05 +0200

    Update to 0.0.3

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,5 @@
-
 #!/bin/bash
 
-cmake -GNinja ${CMAKE_ARGS}
-
-ninja install
+cmake -B build -D XEUS_OCTAVE_GLFW3_OSMESA_BACKEND=ON ${CMAKE_ARGS}
+cmake --build build --parallel ${CPU_COUNT}
+cmake --install build --prefix ${PREFIX} 
